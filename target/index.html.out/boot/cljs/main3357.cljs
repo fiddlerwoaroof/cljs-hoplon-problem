32a1dfa0f14c61143fc1901a11e7609920aba1ee
fiddlerwoaroof <fiddlerwoaroof@gmail.com>
2017-08-13 15:00:17 -0700

    add compiled version

---
+++ target/index.html.out/boot/cljs/main3357.cljs
@@ -0,0 +1,1 @@
+(ns boot.cljs.main3357 (:require hoplon.app-pages._index_DOT_html))
---
+++ target/index.html.out/hoplon/app_pages/_index_DOT_html.js
@@ -0,0 +1,15 @@
+// Compiled by ClojureScript 1.9.293 {:static-fns true, :optimize-constants true}
+goog.provide('hoplon.app_pages._index_DOT_html');
+goog.require('cljs.core');
+goog.require('javelin.core');
+goog.require('hoplon.jquery');
+goog.require('hoplon.core');
+hoplon.core.html.cljs$core$IFn$_invoke$arity$variadic(cljs.core.array_seq([(function (){var G__14416 = (function (){var G__14418 = cljs.core.cst$kw$src;
+var G__14419 = "purify.js";
+return (hoplon.core.script.cljs$core$IFn$_invoke$arity$2 ? hoplon.core.script.cljs$core$IFn$_invoke$arity$2(G__14418,G__14419) : hoplon.core.script.call(null,G__14418,G__14419));
+})();
+var G__14417 = (function (){var G__14420 = document.write(DOMPurify.sanitize("<!--[if gte mso 9]><xml></xml><![endif]--><br/><!--[if gte mso 9]><xml></xml><![endif]--><!--[if gte mso 9]><xml></xml><![endif]--><!--[if gte mso 10]><style></style><![endif]--><div class=\"separator\" style=\"clear: both; text-align: center;\"><script>alert('foo')</script></div>",cljs.core.clj__GT_js(new cljs.core.PersistentArrayMap(null, 2, [cljs.core.cst$kw$FORBID_ATTR,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, ["style"], null),cljs.core.cst$kw$FORBID_TAGS,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, ["style"], null)], null))));
+return (hoplon.core.script.cljs$core$IFn$_invoke$arity$1 ? hoplon.core.script.cljs$core$IFn$_invoke$arity$1(G__14420) : hoplon.core.script.call(null,G__14420));
+})();
+return (hoplon.core.body.cljs$core$IFn$_invoke$arity$2 ? hoplon.core.body.cljs$core$IFn$_invoke$arity$2(G__14416,G__14417) : hoplon.core.body.call(null,G__14416,G__14417));
+})()], 0));
